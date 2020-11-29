a9a1358b7538a8597bda733c5a32340e02e22af8
Joshua Matthews <jmatth93@gmail.com>
2020-11-28 22:39:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,15 @@ def get_field(exif, field):
         if tag_name == field:
             return v
 
+def shutter_speed_format(field_value):
+    return "{}/{}".format(field_value.numerator, field_value.denominator)
+
+def handle_format(name, field_value, fixes):
+    if name == "ExposureTime":
+        return "{}{}{}".format(fixes["prefix"], shutter_speed_format(field_value), fixes["postfix"])
+    else:
+        return "{}{}{}".format(fixes["prefix"], field_value, fixes["postfix"])
+
 def parse_exif_data(exifdata, info):
     output = [None] * len(info)
     for name in info:
@@ -31,7 +40,7 @@ def parse_exif_data(exifdata, info):
         if index >= len(output) or index < 0:
             print("{}'s order must be between 0 and the number of info items you have. Current order value: {}".format(name, order))
             exit()
-        output[fixes["order"]] = "{}{}{}".format(fixes["prefix"], field_value, fixes["postfix"])
+        output[fixes["order"]] = handle_format(name, field_value, fixes)
     return ' | '.join(output)
 
 def create_img_with_border(img, info):
@@ -48,14 +57,17 @@ def add_text(img, infostr, info):
     size = img.size
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(info["ttf_file"], info["point"])
-    draw.text((0,0), infostr, get_color(info["color"]), font=font)
+    text_width = font.getsize(infostr)[0]
+    x_offset = 0.5 * size[0] - (text_width * 0.5)
+    y_offset = 0.04 * size[1]
+    draw.text((x_offset, size[1] - y_offset), infostr, get_color(info["color"]), font=font)
 
 def save_file(filename, img):
     filename = filename.split(".")
     newname = ".".join(filename[:-1])
     img.save(newname + "-edited." + filename[-1])
 
-def fucking_sendit(image_file, info):
+def export_info_image(image_file, info):
     if not (path.exists(image_file) and path.isfile(image_file)):
         print("could not find the given image file")
         exit()
@@ -80,4 +92,4 @@ if __name__ == "__main__":
         print("A JPEG image file and info.json file are required as arguments.")
         exit()
     info = read_info_file(sys.argv[2])
-    fucking_sendit(sys.argv[1], info)
+    export_info_image(sys.argv[1], info)
